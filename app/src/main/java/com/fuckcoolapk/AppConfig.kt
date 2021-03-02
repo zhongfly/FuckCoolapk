409771cd57128f9b51e667ef40775f5b50dc7e6e
ejiaogl <1318566569@qq.com>
2021-03-02 21:46:36 +0800

    Update AppConfig.kt

--- app/src/main/java/com/fuckcoolapk/AppConfig.kt
+++ app/src/main/java/com/fuckcoolapk/AppConfig.kt
@@ -2,4 +2,4 @@ package com.fuckcoolapk
 
 const val PACKAGE_NAME = "com.coolapk.market"
 const val TAG = "FUCKCOOLAPK"
-const val MODULE_TARGET_VERSION = "11.0.2 (2102032)"
+const val MODULE_TARGET_VERSION = "11.0.2 (2102252)"
