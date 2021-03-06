f65814e1d4e08439c05bb04422f8203b4409218e
test <test@gmail.com>
2021-03-07 01:55:17 +0800

    allow uninstall coolapk in coolapk

--- app/src/main/java/com/fuckcoolapk/module/AllowUninstallCoolapk.kt
+++ app/src/main/java/com/fuckcoolapk/module/AllowUninstallCoolapk.kt
@@ -4,6 +4,7 @@ import android.text.TextUtils
 import android.view.View
 import com.fuckcoolapk.PACKAGE_NAME
 import com.fuckcoolapk.utils.CoolapkContext
+import com.fuckcoolapk.utils.LogUtil
 import com.fuckcoolapk.utils.OwnSP
 import com.fuckcoolapk.utils.ktx.hookAfterMethod
 import com.fuckcoolapk.utils.ktx.hookBeforeMethod
@@ -16,7 +17,10 @@ class AllowUninstallCoolapk {
             "com.coolapk.market.view.appmanager.MobileAppFragment\$DataAdapter\$onCreateViewHolder\$1"
                     .hookBeforeMethod("onItemClick", "androidx.recyclerview.widget.RecyclerView\$ViewHolder", View::class.java) {
                         textUtilsHook = "android.text.TextUtils".hookBeforeMethod("equals", CharSequence::class.java, CharSequence::class.java) {
-                            if ((it.args[0] as String) == PACKAGE_NAME) it.result = false
+                            if ((it.args[0] as String) == PACKAGE_NAME) {
+                                it.result = false
+                                LogUtil.toast("干得漂亮（")
+                            }
                         }
                     }
             "com.coolapk.market.view.appmanager.MobileAppFragment\$DataAdapter\$onCreateViewHolder\$1"
