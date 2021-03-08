198524edeb25cd05fd085a9bf8536eb460a686c6
NextAlone <12210746+NextAlone@users.noreply.github.com>
2021-03-08 23:05:12 +0800

    Update RemoveBannerAds.kt

--- app/src/main/java/com/fuckcoolapk/module/RemoveBannerAds.kt
+++ app/src/main/java/com/fuckcoolapk/module/RemoveBannerAds.kt
@@ -1,24 +1,15 @@
 package com.fuckcoolapk.module
 
+import android.util.Log
 import com.fuckcoolapk.utils.CoolapkContext
 import com.fuckcoolapk.utils.OwnSP
-import com.fuckcoolapk.utils.ktx.hookAfterMethod
+import com.fuckcoolapk.utils.ktx.*
 import de.robv.android.xposed.XposedHelpers
 
 class RemoveBannerAds {
     fun init() {
         if (OwnSP.ownSP.getBoolean("removeBannerAds", false)) {
-            XposedHelpers.findClass("com.coolapk.market.view.feed.reply.FeedDetailFragmentV8", CoolapkContext.classLoader)
-                    .hookAfterMethod("updateHeaderData") {
-                        val dataList = XposedHelpers.callMethod(it.thisObject, "getDataList") as List<Any>
-                        XposedHelpers.callMethod(dataList, "remove", dataList.size - 1)
-                    }
-            
-            XposedHelpers.findClass("com.coolapk.market.view.feed.reply.FeedArticleDetailFragment", CoolapkContext.classLoader)
-                    .hookAfterMethod("updateHeaderData") {
-                        val dataList = XposedHelpers.callMethod(it.thisObject, "getDataList") as List<Any>
-                        XposedHelpers.callMethod(dataList, "remove", dataList.size - 1)
-                    }
+            "com.coolapk.market.model.\$\$AutoValue_Feed".findClass(CoolapkContext.classLoader).setReturnConstant("getDetailSponsorCard",result = null)
         }
     }
 }
