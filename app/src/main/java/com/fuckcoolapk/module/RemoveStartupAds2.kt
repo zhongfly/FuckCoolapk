99ded80893d1bf56066333e88c4232e05c426029
ejiaogl <1318566569@qq.com>
2021-03-26 21:25:32 +0800

    Create RemoveStartupAds2.kt

---
+++ app/src/main/java/com/fuckcoolapk/module/RemoveStartupAds2.kt
@@ -0,0 +1,20 @@
+package com.fuckcoolapk.module
+
+import android.content.Context
+import com.fuckcoolapk.utils.CoolapkContext
+import com.fuckcoolapk.utils.LogUtil
+import com.fuckcoolapk.utils.OwnSP
+import com.fuckcoolapk.utils.ktx.setReturnConstant
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedHelpers
+
+class RemoveStartupAds2 {
+    fun init() {
+        if (OwnSP.ownSP.getBoolean("RemoveStartupAds2", false)) {
+            val cDataManager = XposedHelpers.findClass("com.coolapk.market.manager.DataManager", CoolapkContext.classLoader)
+            val dataManager = XposedHelpers.callStaticMethod(cDataManager, "getInstance")
+            val prefEditor = XposedHelpers.callMethod(dataManager, "getPreferencesEditor")
+            XposedHelpers.callMethod(prefEditor, "putLong", "SPLASH_AD_LAST_SHOW", System.currentTimeMillis())
+            XposedHelpers.callMethod(prefEditor, "apply")
+        }
+    }}
