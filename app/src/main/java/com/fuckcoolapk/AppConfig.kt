416fe97975ef6632150b56ad88405242e5e5e525
test <test@gmail.com>
2021-04-03 05:16:43 +0800

    special for Ching Ming Festival fix check update

--- app/src/main/java/com/fuckcoolapk/AppConfig.kt
+++ app/src/main/java/com/fuckcoolapk/AppConfig.kt
@@ -3,4 +3,4 @@ package com.fuckcoolapk
 const val PACKAGE_NAME = "com.coolapk.market"
 const val SP_NAME = "fuckcoolapk"
 const val TAG = "FUCKCOOLAPK"
-const val MODULE_TARGET_VERSION = "11.1.0 (2103251)"
+const val MODULE_TARGET_VERSION = "11.1.2 (2104021)"
