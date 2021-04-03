46c5453d3e2c827b41234ceee770baad67f21fe5
test <test@gmail.com>
2021-04-03 16:04:21 +0800

    add AppFilter.kt (R+)

--- app/src/main/java/com/drakeet/filter/AppFilter.kt
+++ app/src/main/java/com/drakeet/filter/AppFilter.kt
@@ -5,6 +5,7 @@ import android.util.ArraySet
 import androidx.annotation.RequiresApi
 import com.fuckcoolapk.BuildConfig
 import com.fuckcoolapk.utils.LogUtil
+import com.fuckcoolapk.utils.ktx.hookAfterMethod
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
@@ -30,37 +31,26 @@ class AppFilter {
 
     fun init(classLoader: ClassLoader) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            LogUtil.d("\n\n# AppFilter\n---\n")
 
-            findAndHookMethod(
-                    "com.android.server.pm.AppsFilter", classLoader,
-                    "shouldFilterApplication",
-                    Int::class.javaPrimitiveType,
-                    "com.android.server.pm.SettingBase",
-                    "com.android.server.pm.PackageSetting",
-                    Int::class.javaPrimitiveType,
-                    object : XC_MethodHook() {
+            "com.android.server.pm.AppsFilter"
+                    .hookAfterMethod("shouldFilterApplication", Int::class.javaPrimitiveType, "com.android.server.pm.SettingBase", "com.android.server.pm.PackageSetting", Int::class.javaPrimitiveType, classLoader = classLoader) {
+                        if (it.result == true) return@hookAfterMethod
+                        // toString will be faster
+                        // val packageName = on(param.args[2]).field("name").get<String>()
+                        val targetPkgSetting = it.args[2]
+                        val callerSettingBase = it.args[1]
 
-                        override fun afterHookedMethod(param: MethodHookParam) {
-                            if (param.result == true) return
+                        val targetPackageName = targetPkgSetting.packageName
+                        val callerPackageName = callerSettingBase?.packageName ?: "N/A"
 
-                            // toString will be faster
-                            // val packageName = on(param.args[2]).field("name").get<String>()
-                            val targetPkgSetting = param.args[2]
-                            val callerSettingBase = param.args[1]
-
-                            val targetPackageName = targetPkgSetting.packageName
-                            val callerPackageName = callerSettingBase?.packageName ?: "N/A"
-
-                            if (targetPackageName in filteredList && callerPackageName !in callerWhiteList) {
-                                if (!notedSet.contains(callerPackageName)) {
-                                    notedSet.add(callerPackageName)
-                                    LogUtil.d("Filtered: $callerPackageName → $targetPackageName")
-                                }
-                                param.result = true
+                        if (targetPackageName in filteredList && callerPackageName !in callerWhiteList) {
+                            if (!notedSet.contains(callerPackageName)) {
+                                notedSet.add(callerPackageName)
+                                LogUtil.d("AppFilter Filtered: $callerPackageName → $targetPackageName")
                             }
+                            it.result = true
                         }
-                    })
+                    }
         }
     }
 
