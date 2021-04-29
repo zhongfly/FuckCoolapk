696569d93cf03b78fdddc0b4f77a09d21618799b
1767523953 <60041789+1767523953@users.noreply.github.com>
2021-04-29 20:55:32 +0800

    Update app/src/main/java/com/fuckcoolapk/module/DisableUpdateRemind.kt
    Co-authored-by: NextAlone <12210746+NextAlone@users.noreply.github.com>

--- app/src/main/java/com/fuckcoolapk/module/DisableUpdateRemind.kt
+++ app/src/main/java/com/fuckcoolapk/module/DisableUpdateRemind.kt
@@ -6,7 +6,7 @@ import com.fuckcoolapk.utils.ktx.replaceMethod
 class DisableUpdateRemind {
     fun init(){
         if (OwnSP.ownSP.getBoolean("disableUpdateRemind", false)){
-            "com.coolapk.market.CoolMarketApplication".replaceMethod("checkCoolapkUpgradeInfo") { null }
+            "com.coolapk.market.CoolMarketApplication".setReturnConstant("checkCoolapkUpgradeInfo", result = null)
         }
     }
 }
