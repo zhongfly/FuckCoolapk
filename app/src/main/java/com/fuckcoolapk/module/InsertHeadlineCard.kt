465390d21c6047ed3324c456cf9e276a62827736
QQLittleice <1767523953@qq.com>
2021-05-15 22:45:44 +0800

    change code

--- app/src/main/java/com/fuckcoolapk/module/InsertHeadlineCard.kt
+++ app/src/main/java/com/fuckcoolapk/module/InsertHeadlineCard.kt
@@ -32,10 +32,10 @@ import java.util.*
 class InsertHeadlineCard {
     fun init() {
         "com.coolapk.market.viewholder.ImageListCardViewHolder"
-                .hookBeforeMethod("bindTo", Object::class.java) {
+                .hookBeforeMethod("bindTo", Object::class.java) { param ->
                     if ((CoolContext.activity.javaClass.name == "com.coolapk.market.view.splash.SplashActivity") or (CoolContext.activity.javaClass.name == "com.coolapk.market.view.main.MainActivity")) {
-                        val entityCard = it.args[0]
-                        it.args[0] = "com.coolapk.market.model.EntityCard"
+                        val entityCard = param.args[0]
+                        param.args[0] = "com.coolapk.market.model.EntityCard"
                                 .callStaticMethod("builder")
                                 ?.callMethod("setEntityId", entityCard.callMethod("getEntityId"))
                                 ?.callMethod("setEntityFixed", entityCard.callMethod("getEntityFixed"))
@@ -46,6 +46,15 @@ class InsertHeadlineCard {
                                 ?.callMethod("setLastUpdate", entityCard.callMethod("getLastUpdate"))
                                 ?.callMethod("setEntities", (entityCard.callMethod("getEntities") as ArrayList<Any?>)
                                         .apply {
+                                            if (OwnSP.ownSP.getBoolean("removeFeedAds", false)) {
+                                                val listIterator = this.listIterator()
+                                                while (listIterator.hasNext()) {
+                                                    listIterator.next()?.let {
+                                                        if ((it.callMethod("getUrl") as String).contains("/apk/")) listIterator.remove()
+                                                        if ((it.callMethod("getTitle") as String).contains("好物")) listIterator.remove()
+                                                    }
+                                                }
+                                            }
                                             val jsonArray = JSONArray(OwnSP.ownSP.getString("configBannerCard", "[]"))
                                             for (i in 0 until jsonArray.length()) {
                                                 val jsonObject = jsonArray.getJSONObject(i)
@@ -56,15 +65,6 @@ class InsertHeadlineCard {
                                                         ?.callMethod("setUrl", jsonObject.getString("url"))
                                                         ?.callMethod("setPic", jsonObject.getString("pic"))
                                                         ?.callMethod("build"))
-                                            }
-                                            val removeList = mutableListOf<Any>()
-                                            for (item in this) {
-                                                if (item == null) continue
-                                                if ((item.callMethod("getUrl") as String).contains("/apk/")) removeList.add(item)
-                                                if ((item.callMethod("getTitle") as String).contains("好物")) removeList.add(item)
-                                            }
-                                            for (i in removeList) {
-                                                this.remove(i)
                                             }
                                         }
                                 )
